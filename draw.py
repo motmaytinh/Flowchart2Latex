ae0566f1280158bf40189791893a6f904082784b
Tran Ngoc Quy <motmaytinh@gmail.com>
2018-10-31 16:59:47 +0700

    Add draw_edge function

--- draw.py
+++ draw.py
@@ -1,4 +1,6 @@
-def draw_shape(shape, name, relative_pos="", relative_obj=""):
-    block_exp = "{}, {} of= {}".format() if relative_obj != "" else shape
-    return "\node [{}] ({}) {{{}}};".format(block_exp, name, name)
+def draw_node(shape, name, relative_pos="", relative_obj=""):
+    block_exp = "{}, {} of= {}".format(shape, relative_pos, relative_obj) if relative_obj != "" else shape
+    return "\node [{}] ({}) {{{}}};".format(block_exp, name, "placeholder")
 
+def draw_edge(node_from, node_to):
+    return "\path [line] ({}) -- ({})".format(node_from, node_to)
